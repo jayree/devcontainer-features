709f28e65c967cc267ba77d41c595ff85be93d29
jayree <jayree.git@icloud.com>
2026-05-14 23:46:49 +0200

    Refactor plugin installation script
    fix #7

--- src/sf-plugins/install.sh
+++ src/sf-plugins/install.sh
@@ -2,7 +2,7 @@
 
 PLUGINS=${PLUGINS:-""}
 
-SF_PLUGINS=("${PLUGINS//,/ }")
+SF_PLUGINS=(${PLUGINS//,/ })
 
 set -e
 
@@ -11,11 +11,8 @@ if [ -z "${PLUGINS}" ] ; then
     exit 0
 fi
 
-mkdir -p "${_REMOTE_USER_HOME}/.config/sf/"
+su ${_REMOTE_USER} -c "sf plugins trust allowlist add$(printf ' --name %s' "${SF_PLUGINS[@]}")"
 
-echo ${SF_PLUGINS} | jq -Rc 'split(" ") | map(.[0:])' > "${_REMOTE_USER_HOME}/.config/sf/unsignedPluginAllowList.json"
-
-chown ${_REMOTE_USER}:${_REMOTE_USER} "${_REMOTE_USER_HOME}/.config/sf/unsignedPluginAllowList.json"
-su ${_REMOTE_USER} -c "sf plugins install ${SF_PLUGINS}"
+su ${_REMOTE_USER} -c "sf plugins install ${SF_PLUGINS[@]}"
 
 echo "Done!"
